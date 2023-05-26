8db744ba59785c9524eb061a2b7d4e355a6cd646
SigProSeismology <hameh@ubi.pt>
2023-05-26 09:52:57 +0100

    First commit

--- Run_IPIML.py
+++ Run_IPIML.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 10 21:13:03 2021
-@author: shipe
+Created on Tue Aug 10 21:13:03 2023
+@author: Hamzeh Mohammadigheymasi, Peidong Shi
 """
 
 
